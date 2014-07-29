cfada814c0f37e318361ac86991462fab14a99db
Daan Louter <daan.louter@gmail.com>
2014-07-29 23:02:13 +0100

    updates to copy, now live

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ var w;
 var h;
 var circleAmount = 100;
 var svg;
-
 var frequency = 5000; // expressed in miliseconds
 var myInterval = 0;
 
@@ -27,64 +26,69 @@ function init(){
 		;
 
 	var title = project.append('h2').append('p').html(function(d){return d.name}).attr('class','title');
-	var description = project.append('p').html(function(d){return d.description}).attr('class','description');
+	var description = project.append('p').html(function(d){return d.description + " »"}).attr('class','description');
 	var atGuardian = title.append("span").attr('class','guardian')
 		.html(function(d){
-			if(d.atGuardian) return "GUARDIAN";
+			var string = "";
+			string = d.part;
+			if(d.atGuardian){
+				string = string + ", at The Guardian"
+			}
+			return string;
 		})
 
 	w = $('body').width();
 	h = $(window).height();
-	visualise();
+	// visualise();
 }
 
-function startLoop() {
-	updateData();
-window.setInterval(function(){
-  updateData();
-}, 20000);
-}
+// function startLoop() {
+// 	updateData();
+// window.setInterval(function(){
+//   updateData();
+// }, 20000);
+// }
 
-function visualise(){
-	svg = d3.select('body').append('div').attr('class','background').append('svg').attr('width',w).attr('height',h);
-	svg.selectAll('circle')
-		.data(fillDataset())
-		.enter()
-		.append("circle")
-		.attr('cx',function(d){
-			return d[0];
-		})
-		.attr('cy',function(d){
-			return d[1];
-		})
-		.attr('r',1)
-		.style('opacity', function(d){return Math.random()*0.5})
-	startLoop();
+// function visualise(){
+// 	svg = d3.select('body').append('div').attr('class','background').append('svg').attr('width',w).attr('height',h);
+// 	svg.selectAll('circle')
+// 		.data(fillDataset())
+// 		.enter()
+// 		.append("circle")
+// 		.attr('cx',function(d){
+// 			return d[0];
+// 		})
+// 		.attr('cy',function(d){
+// 			return d[1];
+// 		})
+// 		.attr('r',1)
+// 		.style('opacity', function(d){return Math.random()*0.5})
+// 	startLoop();
 	
-}
+// }
 
-function updateData(){
-	console.log('doei');
-	svg.selectAll('circle')
-		.data(fillDataset())
-		.transition()
-		.duration(20000)
-		.ease('linear')
-		.attr('cx',function(d){
-			return d[0];
-		})
-		.attr('cy',function(d){
-			return d[1];
-		})
+// function updateData(){
+// 	console.log('doei');
+// 	svg.selectAll('circle')
+// 		.data(fillDataset())
+// 		.transition()
+// 		.duration(20000)
+// 		.ease('linear')
+// 		.attr('cx',function(d){
+// 			return d[0];
+// 		})
+// 		.attr('cy',function(d){
+// 			return d[1];
+// 		})
 
-}
+// }
 
 
 
-function fillDataset(){
-	dataset = [];
-	for(var i=0; i<circleAmount; i++){
-		dataset[i] = [Math.floor(Math.random()*w), Math.floor(Math.random()*h)];
-	}
-	return dataset;
-}
+// function fillDataset(){
+// 	dataset = [];
+// 	for(var i=0; i<circleAmount; i++){
+// 		dataset[i] = [Math.floor(Math.random()*w), Math.floor(Math.random()*h)];
+// 	}
+// 	return dataset;
+// }
